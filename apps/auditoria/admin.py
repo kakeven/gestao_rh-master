d24a16fda83047acb5b9f023e14a8925e6889b08
Felipe Henrique <fiphg49@gmail.com>
2026-06-20 16:09:50 -0300

    Corrigido parte do admin, para não adicionar e modificar os logs

--- apps/auditoria/admin.py
+++ apps/auditoria/admin.py
@@ -15,6 +15,9 @@ class LogAuditoriaAdmin(admin.ModelAdmin):
     readonly_fields = ('usuario', 'acao', 'descricao', 'data_hora')
     actions         = None 
 
-#Aqui é para remover o botao padrao de adicionar do propio Django, para não adicionar logs, apenas receber
+#Aqui é para remover o botao padrao de adicionar do propio Django, para não adicionar logs e a outra para que não seja modificado, apenas receber
     def has_add_permission(self, request):
+        return False
+    
+    def has_change_permission(self, request, obj=None):
         return False
